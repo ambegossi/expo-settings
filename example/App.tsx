314be4f9b925d6dd5bf180d35d053d5862d80dd0
Anderson Begossi <andersonbegossi@gmail.com>
2023-08-09 20:40:14 -0300

    feat: get, set, and persist the theme preference value

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import * as Settings from "expo-settings";
 
-import * as ExpoSettings from "expo-settings";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const theme = Settings.getTheme();
+
+  const nextTheme = theme === "dark" ? "light" : "dark";
+
   return (
     <View style={styles.container}>
-      <Text>Theme: {ExpoSettings.getTheme()}</Text>
+      <Text>Theme: {Settings.getTheme()}</Text>
+
+      <Button
+        title={`Set theme to ${nextTheme}`}
+        onPress={() => Settings.setTheme(nextTheme)}
+      />
     </View>
   );
 }
